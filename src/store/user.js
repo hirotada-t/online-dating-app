6bd6f743de72151e654a5781e787101df4c5169d
Hirotada Takeuchi <t.hir0tada.biz@gmail.com>
2022-10-27 23:13:01 +0900

    feat modules

---
+++ src/store/user.js
@@ -0,0 +1,19 @@
+const state = {
+}
+
+const mutations = {
+}
+
+const getters = {
+}
+
+const actions = {
+}
+
+export default {
+  namespaced:true,
+  state,
+  mutations,
+  getters,
+  actions
+}
